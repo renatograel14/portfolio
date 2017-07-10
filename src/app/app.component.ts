6360c48c2cc6b625a93d0ee960a9eda3bd8af3dc
Renato Grael Teixeira <renato.grael@gmail.com>
2017-07-10 18:24:14 -0300

    menu responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menuStatus: boolean = true;
+
+  toggleMenu() {
+    this.menuStatus = !this.menuStatus;
+    console.log(this.menuStatus);
+ }
 }
